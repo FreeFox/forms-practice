bbb910eb38a67715c66fa173c341d27de8264ec4
Roman Lysiuk <roman.lysiuk@osf.digital>
2022-12-20 01:48:48 +0200

    using state for input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+
+  function handleChange (event) {
+    event.target.name === 'first_name' ? setFirstName(event.target.value) : setLastName(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" name="first_name" placeholder='First name' onChange={handleChange} />
+        <input type="text" name="last_name" placeholder='Last name' onChange={handleChange} />
+      </form>
     </div>
   );
 }
