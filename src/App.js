d44e089ef7ee6005ebd75d46a195f1a7ae33bff5
Roman Lysiuk <roman.lysiuk@osf.digital>
2022-12-21 01:51:55 +0200

    for challenge

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import React from 'react';
 
 function App() {
   const [formData, setFormData] = React.useState({
-    firstName:"",
-    lastName:"",
     email: "",
-    comment: "",
-    check: false,
-    employment: "",
-    color: ""
+    password: "",
+    confirm_password: "",
+    newsletter: false
   });
-
-  console.log(formData);
 
   function handleChange (event) {
     const {name, value, type, checked} = event.target;
@@ -24,23 +19,24 @@ function App() {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (formData.password !== formData.confirm_password) {
+      console.log("Passwords to not match");
+      return;
+    } else {
+      console.log("Successfully signed up");
+    }
+
+    if (formData.newsletter) {
+      console.log("Thanks for signing up to our newsletter!");
+    }
+  }
+
   return (
     <div className="App">
-      <form>
-        <input 
-          type="text"
-          name="firstName"
-          placeholder="First name"
-          value={formData.firstName}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Last name"
-          value={formData.lastName}
-          onChange={handleChange}
-        />
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="email"
@@ -48,61 +44,31 @@ function App() {
           value={formData.email}
           onChange={handleChange}
         />
-        <textarea
-          value={formData.comment}
-          name="comment"
-          placeholder="Comment"
+        <input
+          type="text"
+          name="password"
+          placeholder="Password"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="confirm_password"
+          placeholder="Confirm password"
+          value={formData.lastName}
           onChange={handleChange}
         />
         <input
           type="checkbox"
           checked={formData.check}
-          name="check"
-          id="check"
+          name="newsletter"
+          id="newsletter"
           className="d-inline"
           onChange={handleChange}
         />
-        <label htmlFor="check">Check</label>
-        <fieldset>
-          <input
-            type="radio"
-            name="employment"
-            value="full-time"
-            id="full-time-employment"
-            checked={formData.employment === "full-time"}
-            onChange={handleChange}
-            />
-          <label htmlFor="full-time-employment">Full-time</label>
-          <br />
-          <input
-            type="radio"
-            name="employment"
-            value="part-time"
-            id="part-time-employment"
-            checked={formData.employment === "part-time"}
-            onChange={handleChange}
-            />
-          <label htmlFor="part-time-employment">Part-time</label>
-          <br />
-          <input
-            type="radio"
-            name="employment"
-            value="unemployed"
-            id="unemployed-employment"
-            checked={formData.employment === "unemployed"}
-            onChange={handleChange}
-            />
-          <label htmlFor="unemployed-employment">Unemployed</label>
-        </fieldset>
-        <select
-          name="color"
-          value={formData.color}
-          onChange={handleChange}
-        >
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="blue">Blue</option>
-        </select>
+        <label htmlFor="newsletter">I want to join the newsletter</label>
+        <br />
+        <button type="submit">Sign up</button>
       </form>
     </div>
   );
